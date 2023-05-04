80ae11ccce3acce9ba5287838c3702ab066a6999
Muhammad Hamza Shaikh <hamzashaikhwork@gmail.com>
2023-05-04 12:33:03 +0500

    Class API created

--- backend/router/class.js
+++ backend/router/class.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const app = express();
-const classController = require('../controller/user');
+const classController = require('../controller/class');
 
 
 // Add a new class
-app.post('/add', classController.signinUser);
+app.post('/add', classController.createNewClass);
+
+
+// Add student to class
+app.post('/add/student', classController.addStudentToClass);
 
 // Get a new class
-app.get('/get/:classID', classController.signinUser);
+app.get('/get/:teacherid', classController.getTeacherClass);
 
 
 
 
 
 
-// e.g: http://localhost:5005/api/class/add   [POST: send "name","teacherID","students","classCode" of user to this API]
-// e.g: http://localhost:5005/api/class/get/:classID   [GET]
-// e.g: http://localhost:5005/api/user/create   [POST: send "fullName","email","password","role" of user to this API]
+// e.g: http://localhost:5005/api/class/add   [POST: send "name","teacherID","classCode"  to this API]
+// e.g: http://localhost:5005/api/class/add/student   [POST: send "studentID","classCode" to this API]
+// e.g: http://localhost:5005/api/class/get/:teacherid [GET]
 
 
 module.exports = app;
